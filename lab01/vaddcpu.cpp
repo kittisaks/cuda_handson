f1178e387a6001dbac4aa7611a09a62fa567a17c
kittisak <kittisak.sajjapongse@mail.missouri.edu>
2016-12-16 08:16:04 -0500

    Implemented vaddcpu

--- lab01/vaddcpu.cpp
+++ lab01/vaddcpu.cpp
@@ -1,27 +1,59 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "bin_reader.h"
 #include "timer.h"
 
+#define CHECK(exp)                                          \
+    do {                                                    \
+        if (exp != 0) {                                     \
+            printf("Runtime error at line %d\n", __LINE__); \
+            exit(-1);                                       \
+        }                                                   \
+    } while(0)
+
 int main(int argc, char ** argv) {
 
-#if 0
-    float * vectorA;
-    binReadAsArray<float>("gold/vecA.bin", NULL, &vectorA);
+    float * veca_h, * vecb_h, * vecc_h, * vecc_hv;
+    size_t  count;
 
-    for (size_t idx=0;idx<1000;idx++)
-        printf("%.4f\n", vectorA[idx]);
-#else
-    size_t count;
-    float * vectorA;
-    BinInfo bi;
-    if (binReadAsArrayNP<float>("gold/vecA.bin", &bi, &vectorA, &count) == -1)
-        printf("ERROR!\n");
+    //Loading vector-A and -B from file for calculation on GPU
+    //Loading vector-C from file for verifying the results from GPU
+    CHECK(    binReadAsArrayNP<float>("vecA.bin", NULL, &veca_h, &count));
+    CHECK(    binReadAsArrayNP<float>("vecB.bin", NULL, &vecb_h, &count));
+    CHECK(    binReadAsArrayNP<float>("vecC.bin", NULL, &vecc_hv, &count));
+    vecc_h = new float [count];
 
-    for (size_t idx=0;idx<1000;idx++)
-        printf("%.4f\n", vectorA[idx]);
 
-#endif
+    //Perform the vector addition by calling the kernel
+    //Play with scaling and configuration (WBB)
+    Timer compTimer;
+    compTimer.Start();
+
+    omp_set_num_threads(4);
+
+    #pragma parallel for shared(vecc_h, veca_h, vecb_h)
+    for (size_t i=0;i<count;i++)
+        vecc_h[i] = veca_h[i] + vecb_h[i];
+
+    compTimer.Stop();
+    Timer::Duration d = compTimer.GetDuration();
+    printf("Computation time: %.2f us\n", d.raw);
+
+
+    //Verification
+    for (size_t idx=0;idx<count;idx++) {
+        if (vecc_h[idx] != vecc_hv[idx]) {
+            printf("Verification: FAILED (%ld)\n", idx);
+            exit(-1);
+        }
+    }
+    printf("Verification: PASSED\n");
+
+    //Release resources after completed calculation (WBB)
+    CHECK(    binDiscardArrayNP(veca_h));
+    CHECK(    binDiscardArrayNP(vecb_h));
+    CHECK(    binDiscardArrayNP(vecc_hv));
 
     return 0;
 }
